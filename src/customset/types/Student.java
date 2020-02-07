e3545d8e8ca3e778ae762a3daef11a3d0a3bd0a2
Vahe Odabashyan <vaheodabashyan@gmail.com>
2020-02-07 23:22:26 +0400

    added Student class

---
+++ src/customset/types/Student.java
@@ -0,0 +1,64 @@
+package customset.types;
+
+import java.util.Objects;
+
+public class Student implements Comparable<Student> {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+
+    @Override
+    public int compareTo(Student student1) {
+        return firstName.compareTo(student1.firstName);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return firstName.equals(student.firstName) &&
+                lastName.equals(student.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+}
